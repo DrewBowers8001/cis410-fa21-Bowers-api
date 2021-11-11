be8e6885829fcd7b57673bfe1a22e94b77be5967
unknown <drew06@colostate.edu>
2021-11-11 12:24:38 -0700

    11/3 post hashed passwords workd=ed on /Shifts end point

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express")
-
+const bcrypt = require("bcryptjs")
 const db = require("./dbconnectExec.js")
 const app = express(); 
+app.use(express.json());
+
 
 app.listen(5000,() => {
     console.log(" app is running on port 5000")
@@ -16,6 +18,43 @@ app.get("/hi",(req, res) => { res.send("hello world ")
 app.get("/", (req, res) => { res.send("API is running ")})
 
 
+
+app.post("/Shifts", async (req, res) => {
+    // res.send("/Customer called");
+  
+    // console.log("request body", req.body);
+  
+    let day = req.body.day;
+    let start_time = req.body.start_time;
+    let time = req.body.time;
+    let end_time = req.body.end_time;
+    let AvailibilityPK = req.body.AvailibilityPK;
+    let  EmployeePK = req.body.EmployeePK;
+    
+  
+    if (!day || !start_time || !time || !end_time || !AvailibilityPK|| !EmployeePK ) {
+      return res.status(409).send("Bad request");
+   }
+    
+  
+  let timeCheckQuery = `SELECT EmployeePK From Shifts Where EmployeePK = ${EmployeePK}`;
+
+  let existingUser = await db.executeQuery(timeCheckQuery);
+    if(existingUser[0]){ return res.status(409).send("Duplicate time")};
+    let hashedID = bcrypt.hashSync(EmployeePK)
+    let insertQuery = ` INSERT INTO Shifts(day, start_time ,time , end_time , AvailibilityPK , EmployeePK)
+    VALUES('${day}',${start_time},${time},${end_time},${AvailibilityPK},${hashedID})`
+
+    db.executeQuery(insertQuery) 
+    .then(()=> {
+        res.status(201).send();
+    })
+    .catch((err) => {
+        console.log("Error in post", err);
+        res.status(500).send 
+    })
+});
+
 app.get("/Employee", (req,res ) => {
 
     db.executeQuery(
@@ -29,7 +68,9 @@ app.get("/Employee", (req,res ) => {
     
     res.status(500).send(); 
     })
-});
+}); 
+ 
+
 
 app.get("/Employee/:pk", (req, res) => { 
     let pk = req.params.pk
